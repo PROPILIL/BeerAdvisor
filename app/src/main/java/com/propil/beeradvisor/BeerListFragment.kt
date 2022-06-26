6ab58db9fb3e5d9c5f363a58c58a4186d53e2aa8
PROPILIL <83790466+PROPILIL@users.noreply.github.com>
2022-06-26 23:17:57 +0300

    1. There's no NPE now

--- app/src/main/java/com/propil/beeradvisor/BeerListFragment.kt
+++ app/src/main/java/com/propil/beeradvisor/BeerListFragment.kt
@@ -1,5 +1,6 @@
 package com.propil.beeradvisor
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -17,6 +18,11 @@ private const val TAG = "BeerListFragment"
 
 class BeerListFragment: Fragment() {
 
+    interface Callbacks {
+        fun onBeerSelected(beerId: Long)
+    }
+
+    private var callbacks: Callbacks? = null
     private lateinit var beerRecyclerView: RecyclerView
     private lateinit var beerModel: BeerModel
     private var adapter: BeerAdapter? = BeerAdapter(emptyList())
@@ -26,21 +32,26 @@ class BeerListFragment: Fragment() {
         ViewModelProvider(this)[BeerViewModel::class.java]
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         inflater.inflate(R.layout.fragment_beer_list, container, false)
-
-        beerRecyclerView = view?.findViewById(R.id.beer_recycler_view) as RecyclerView
-        beerRecyclerView.layoutManager = GridLayoutManager(context, 2)
-
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        beerRecyclerView = view.findViewById(R.id.beer_recycler_view) as RecyclerView
+        beerRecyclerView.layoutManager = GridLayoutManager(context, 2)
+
         beerListViewModel.beerListLiveData.observe(viewLifecycleOwner) { beerModel ->
             beerModel?.let {
                 Log.i(TAG, "Got beers ${beerModel.size}")
@@ -49,10 +60,20 @@ class BeerListFragment: Fragment() {
         }
     }
 
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
+    }
+
     private fun updateUI(beer: List<BeerModel>){
         adapter = BeerAdapter(beer)
         beerRecyclerView.adapter = adapter
     }
+
+    //-----------------------------------------------------------------------------------------------------------//
+    //-----------------------------------------------------------------------------------------------------------//
+    //--------------------------------BEER  VIEWHOLDER-------------------------------------------------------------//
+    //-----------------------------------------------------------------------------------------------------------//
     private inner class BeerViewHolder(view: View)
         :RecyclerView.ViewHolder(view), View.OnClickListener {
 
@@ -71,10 +92,13 @@ class BeerListFragment: Fragment() {
         }
 
         override fun onClick(p0: View?) {
-            Log.d(TAG, "${beerModel.beerName} selected")
+            callbacks?.onBeerSelected(this.beer.id)
         }
     }
-
+    //-----------------------------------------------------------------------------------------------------------//
+    //-----------------------------------------------------------------------------------------------------------//
+    //--------------------------------BEER  ADAPTER-------------------------------------------------------------//
+    //-----------------------------------------------------------------------------------------------------------//
 
     private inner class BeerAdapter(var allBeers: List<BeerModel>)
         :RecyclerView.Adapter<BeerViewHolder>(){
